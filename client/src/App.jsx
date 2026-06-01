492a5a91085f4d951c3cb40a39d6e7f99055fa5c
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2026-06-01 15:04:32 +0530

    feat: add starfield 3D background with neon green theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,13 +11,15 @@ import Marquee from './components/Marquee';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import AdminPage from './components/AdminPage';
+import StarfieldBackground from './components/StarfieldBackground';
 import './index.css';
 
 function Portfolio() {
   return (
     <>
+      <StarfieldBackground />
       <Navbar />
-      <main>
+      <main style={{ position: 'relative', zIndex: 1 }}>
         <Hero />
         <Marquee />
         <About />
