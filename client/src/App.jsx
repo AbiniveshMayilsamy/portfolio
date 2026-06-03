165de9538baadf4256efdb29096d18ec639e8f57
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2026-06-03 14:01:09 +0530

    feat: install and configure Vercel Web Analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -45,6 +46,7 @@ function App() {
         <Route path="/" element={<Portfolio />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
